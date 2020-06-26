c6d0965fdaa3820dc4aa88c9e90dfb24a289cd4a
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-06-26 15:44:28 +0530

    Update main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -8,6 +8,13 @@ class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return new MaterialApp(
+      home: new Material(
+        child: new Center(
+          child: new Text("Hello World!"),
+        ),
+      ),
+    );
     throw UnimplementedError();
   }
   
