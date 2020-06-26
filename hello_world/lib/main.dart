5f37d7b941f2fde4871f1ad988a8ed3d504e6105
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-06-26 15:31:40 +0530

    Initial Commit
    Flutter implementations

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HelloWorld()); //Entry point to the app
+}
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
