7a9cf172a6ab0e942158b2c80d69eec58c94e01e
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-07-04 22:03:32 +0530

    Initial commit

---
+++ web_view/android/app/src/main/java/com/agambajwa/webview/MainActivity.java
@@ -0,0 +1,6 @@
+package com.agambajwa.webview;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ web_view/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      routes: {
+        "/": (_) => new WebviewScaffold(
+          url: "https://www.google.com",
+          appBar: new AppBar(
+            title: new Text("Widget webview"),
+          ),
+        ),
+      },
+    );
+  }
+}
